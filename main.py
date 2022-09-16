ce20547768a87e1ebe539209f3b28ef6582b6010
x86Cow <62828198+x86Cow@users.noreply.github.com>
2022-09-16 01:53:31 +0000

    Added password encryption and write to file

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import string
 import random
+import base64
 
 def passwordGen(i):
-  randomStr =   ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(i))
-  print(randomStr)
-passwordGen(20)
+  randomStr =   ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(i))
+  hash = base64.b64encode(randomStr.encode("utf-8"))
+  hash = base64.b64decode(hash).hex()
+  return(hash)
+
+def passwordSave(hash):
+  f = open("password.pass", 'a')
+  f.write(hash + '\n')
+  f.close()
+def passwordOpen():
+  f = open("password.pass", 'r')
+  return(f.read())
+  f.close()
+
+encryptedPass = passwordGen(20)
+passwordSave(encryptedPass)
+print(passwordOpen())
