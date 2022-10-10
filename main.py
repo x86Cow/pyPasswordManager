12437d67fd878e22693da07876a969b7d4fe7d13
x86Cow <62828198+x86Cow@users.noreply.github.com>
2022-10-10 14:35:29 +0000

    argparse

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import string
 import random
 import base64
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", default=0, help="generate password", type=int)
+parser.add_argument("-o", action='store_true' , help="view passwords")
+parser.parse_args()
+args = parser.parse_args()
 
 def passwordGen(i):
   randomStr =   ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(i))
@@ -8,15 +15,17 @@ def passwordGen(i):
   hash = base64.b64decode(hash).hex()
   return(hash)
 
-def passwordSave(hash):
+def passwordSave(hash, label):
   f = open("password.pass", 'a')
-  f.write(hash + '\n')
+  f.write(label + ": " + hash + '\n')
   f.close()
 def passwordOpen():
   f = open("password.pass", 'r')
   return(f.read())
   f.close()
 
-encryptedPass = passwordGen(20)
-passwordSave(encryptedPass)
-print(passwordOpen())
+label = "Hello World"
+if args.g:
+  passwordSave(passwordGen(args.g), label)
+if args.o:
+  print(passwordOpen())
