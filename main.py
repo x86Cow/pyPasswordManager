acbf2775a8635aaaa1b1d2b1a26693db428fda6f
Matthew  Edelen <84196639+mteam88@users.noreply.github.com>
2022-10-12 16:52:24 +0000

    clean & tidy

--- main.py
+++ main.py
@@ -5,27 +5,35 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-g", default=0, help="generate password", type=int)
-parser.add_argument("-o", action='store_true' , help="view passwords")
+parser.add_argument("-o", action='store_true', help="view passwords")
 parser.parse_args()
 args = parser.parse_args()
 
-def passwordGen(i):
-  randomStr =   ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(i))
-  hash = base64.b64encode(randomStr.encode("utf-8"))
-  hash = base64.b64decode(hash).hex()
-  return(hash)
+PASSWORD_FILE = "password.pass"
+LABEL = "Hello World"
 
-def passwordSave(hash, label):
-  f = open("password.pass", 'a')
-  f.write(label + ": " + hash + '\n')
-  f.close()
-def passwordOpen():
-  f = open("password.pass", 'r')
-  return(f.read())
-  f.close()
+def password_gen(i):
+    'Generate password'
+    random_str = ''.join(random.SystemRandom().choice(
+        string.ascii_letters + string.digits + string.punctuation) for _ in range(i))
+    random_hash = base64.b64encode(random_str.encode("utf-8"))
+    random_hash = base64.b64decode(random_hash).hex()
+    return random_hash
 
-label = "Hello World"
+
+def password_save(password_hash, label):
+    'Save password to file'
+    with open(PASSWORD_FILE, 'a', encoding='utf-8') as pass_file:
+        pass_file.write(label + ": " + password_hash + '\n')
+
+
+def password_open():
+    'Get password from file'
+    with open(PASSWORD_FILE, 'r', encoding='utf-8') as pass_file:
+        return pass_file.read()
+
+
 if args.g:
-  passwordSave(passwordGen(args.g), label)
+    password_save(password_gen(args.g), LABEL)
 if args.o:
-  print(passwordOpen())
+    print(password_open())
